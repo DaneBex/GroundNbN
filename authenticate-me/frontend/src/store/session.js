30292c03012464432edb305516cbce279a0e59c0
Dane Becker <danebecker790@gmail.com>
2022-02-03 17:04:03 -0600

    heroku setup

--- authenticate-me/frontend/src/store/session.js
+++ authenticate-me/frontend/src/store/session.js
@@ -1,47 +1,81 @@
-import { bindActionCreators } from "redux"
-import { csrfFetch } from "./csrf"
+import { csrfFetch } from './csrf';
 
-const ADD_SESSION = '/store/session'
-const REMOVE_SESSION = '/store/session'
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
 
-const makeSession = (user) => {
-return {user,
-    type: ADD_SESSION
-}
-}
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user,
+    };
+};
 
-const removeSession = () => {
-return {type: REMOVE_SESSION}
-}
+const removeUser = () => {
+    return {
+        type: REMOVE_USER,
+    };
+};
 
-export const setSession = (credential, password) => async dispatch => {
-const response = await csrfFetch('/api/session', {
-    method: 'post',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({credential, password}),
-})
+export const login = (user) => async (dispatch) => {
+    const { credential, password } = user;
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
 
-if (response.ok) {
-    const user = await response.json()
-    dispatch(makeSession(user))
-}
-}
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
 
-const initialState = {}
+export const signup = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            email,
+            password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
 
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+};
 
-export default function sessionReducer(state = initialState, action) {
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
     let newState;
     switch (action.type) {
-        case ADD_SESSION:
-            newState = {}
-            newState.user = action.user
+        case SET_USER:
+            newState = Object.assign({}, state);
+            newState.user = action.payload;
             return newState;
-        case REMOVE_SESSION:
-            newState = {};
+        case REMOVE_USER:
+            newState = Object.assign({}, state);
             newState.user = null;
-            return newState
+            return newState;
         default:
             return state;
     }
-}
+};
+
+export default sessionReducer;
