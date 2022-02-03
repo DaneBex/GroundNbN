bec4f6b62918a51bb81b934cce3a33cd70e8d1d3
Dane Becker <danebecker790@gmail.com>
2022-02-03 14:43:55 -0600

    setup frontend

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -10,10 +10,6 @@ const router = require('express').Router()
 router.use('/session', sessionRouter)
 router.use('/users', usersRouter)
 
-router.post('/test', (req, res) => {
-    res.json({requestBody: req.body})
-})
-
 
 
 module.exports = router
