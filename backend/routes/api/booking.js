81f83fca09943de7554421e004410c61b1c2fa54
Dane Becker <danebecker790@gmail.com>
2022-02-09 14:25:55 -0600

    backend booking change

--- backend/routes/api/booking.js
+++ backend/routes/api/booking.js
@@ -1,10 +1,20 @@
 const express = require('express')
-const { Booking } = require('../../db/models')
+const { Booking, Place } = require('../../db/models')
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
     const bookings = await Booking.findAll();
+    const vals = bookings.map(booking => booking.dataValues)
+    res.json(vals)
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    const bookings = await Booking.findAll({
+        where: { userId: id },
+        include: Place
+    })
     const vals = bookings.map(booking => booking.dataValues)
     res.json(vals)
 })
